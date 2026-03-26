b7abc60b7d05ff20ce7935903e929e72c8863863
RealRottale1 <camarkham2@gmail.com>
2026-03-26 08:05:41 -0400

    Update test_inventory.py

--- test_inventory.py
+++ test_inventory.py
@@ -1,6 +1,7 @@
 import pytest
 from inventory import add_item, remove_item, get_item_count
 
+
 def empty_inventory():
     return {"items": [], "capacity": 10, "locked": False}
 
@@ -39,8 +40,6 @@ def test_remove_item_full():
     item = "a"
     assert len(remove_item(full_inventory, item)["items"]) == 9
 
-
 def test_remove_item_locked():
     item = "sword"
     assert item in remove_item(locked_inventory, item)["items"]
-
